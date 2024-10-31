02ab2a22fc12b41ad281dbe2b1772961f6be1f00
drmujumdarclinic.github.io <sumedhmujumdar@gmail.com>
2024-10-31 10:54:42 +0530

    Update Appointment.js

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    date: Date,
+    time: String,
+});
+
+module.exports = mongoose.model('Appointment', appointmentSchema);
 
